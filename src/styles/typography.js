6cabf526553a2f8415f014f880ed4cb663ec46df
Mel <jones.mel@gmail.com>
2020-01-16 15:37:12 +0100

    h1 reduce font-weight, color grey

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,11 +1,14 @@
 import css from '@emotion/css';
 import dimensions from "styles/dimensions";
+import colors from "styles/colors";
+
 
 const typeStyles = css`
     h1 {
-        font-size: 2.8em;
+        font-size: 2em;
         line-height: 1.45;
-        font-weight: 800;
+        font-weight: 300;
+        color: ${colors.grey700};
 
         @media(max-width:${dimensions.maxwidthTablet}px) {
             font-size: 2.25em;
